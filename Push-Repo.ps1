ae44a02ca1d2d806035666d9a3c67f7841482af0
nwrobel <nickwrobel2@gmail.com>
2019-02-19 17:20:34 -0500

    testing repo push script

--- Push-Repo.ps1
+++ Push-Repo.ps1
@@ -1,12 +1,24 @@
 $commitMsg = Read-Host -Prompt "Enter commit message"
 
-$PSProfilePath = (Split-Path -Path $profile -Parent) + "\profile.ps1"
-$Destination = (Split-Path -Path $PSScriptRoot -Parent)
+$PSProfilePathSource = (Split-Path -Path $profile -Parent) + "\profile.ps1"
+$PSProfilePathDestination = (Split-Path -Path $PSScriptRoot -Parent) + "\profile.ps1"
 
-Copy-Item -Path $PSProfilePath -Destination $Destination
+Copy-Item -Path $PSProfilePathSource -Destination $PSProfilePathDestination -Verbose
+
+if (Test-Path -Path $PSProfilePathDestination) {
+    Write-Host "Powershell profile copied into repo successfully"
+} else {
+    throw "Error: Powershell profile was not found in the repo: exiting"
+}
 
 git add --all
 git commit -m $commitMsg
 git push
 
-Remove-Item -Path "$Destination\profile.ps1" -WhatIf
+Remove-Item -Path $PSProfilePathDestination -Verbose
+
+if (Test-Path -Path $PSProfilePathDestination) {
+    throw "Error: Powershell profile was not removed from the repo: please remove it manually"
+} else {
+    Write-Host "Powershell profile copy was removed from the repo successfully"
+}
