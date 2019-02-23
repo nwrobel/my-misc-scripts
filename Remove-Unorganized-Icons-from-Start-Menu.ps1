1432b9c48d72bbf3df3ea0632a36a696b1ca3b90
nwrobel <nickwrobel2@gmail.com>
2019-02-22 21:06:18 -0500

    fixed the start menu cleanup script

--- Remove-Unorganized-Icons-from-Start-Menu.ps1
+++ Remove-Unorganized-Icons-from-Start-Menu.ps1
@@ -1,4 +1,25 @@
-﻿$expectedStartMenuSubfolderNames = @(
+﻿function Assert-ReadInputYesToProceed {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Prompt,
+      
+        [Parameter(Mandatory=$false)]
+        [string]$ErrorMessageOnInputNo = "Stopping script due to user choice"
+    )
+
+    do {
+        $continue = Read-Host -Prompt "$Prompt [Y/N]"
+
+        if ($continue -eq 'n') {
+            Write-Host $ErrorMessageOnInputNo -ForegroundColor Red
+        
+        } elseif ($continue -ne 'y') {
+            Write-Host "Incorrect input: Enter Y or N" -ForegroundColor Red
+        }
+    } while ($continue -ne 'y')   
+}
+
+$expectedStartMenuSubfolderNames = @(
 'Audio Tools',
 'Development',
 'Drawing & Modeling',
@@ -20,24 +41,28 @@
 'Windows System'
 )
 
+Write-Host "Organizing start menu according to the list of expected start menu subfolders specified" -ForegroundColor Green
+Write-Host "Scanning for items to remove..." -ForegroundColor Cyan
+
 $currentUserStartMenuPath = "$($ENV:APPDATA)\Microsoft\Windows\Start Menu\Programs"
 $systemStartMenuPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
 
 Write-host "Warning - this will delete:`n- ALL shortcuts/folders from the current user start menu folder`n- All unsorted shortcuts/folders (those not in the expected subfolders) from the system start menu" -ForegroundColor Magenta
 
-$currentUserStartMenuItemsToDelete = Get-ChildItem -Path $currentUserStartMenuPath -Recurse
-$systemStartMenuItemsToDelete = Get-ChildItem -Path $systemStartMenuPath -Recurse -Exclude $expectedStartMenuSubfolders
+$expectedStartMenuSubfolders = ($expectedStartMenuSubfolderNames | ForEach-Object  {
+    Join-Path -Path $systemStartMenuPath -ChildPath $_
+})
 
-Write-Host "The following items are set to be deleted:" -ForegroundColor Magenta
-$currentUserStartMenuItemsToDelete
-$systemStartMenuItemsToDelete
+$currentUserStartMenuItemsToDelete = (Get-ChildItem -Path $currentUserStartMenuPath)
+$systemStartMenuItemsToDelete = (Get-ChildItem -Path $systemStartMenuPath | Where-Object -FilterScript { $_.FullName -notin $expectedStartMenuSubfolders})
 
-$continue = Read-Host -Prompt "Are you sure you want to remove these items? [y/n]"
+Write-Host "The following start menu items are ready to be deleted:" -ForegroundColor Red
+$currentUserStartMenuItemsToDelete | Select-Object -ExpandProperty FullName 
+$systemStartMenuItemsToDelete | Select-Object -ExpandProperty FullName 
 
-if ($continue -eq 'y') {
+Assert-ReadInputYesToProceed -Prompt "Are you sure you want to delete these items?"
 
-} elseif ($continue -eq 'n') {
+$currentUserStartMenuItemsToDelete | Remove-Item -Force -Verbose -WhatIf -Recurse
+$systemStartMenuItemsToDelete | Remove-Item -Force -Verbose -WhatIf -Recurse
 
-} else {
-    Write-Host 'Please enter y or n to decide' -ForegroundColor Red
-}
+Write-Host "Unorganized start menu items were removed successfully!" -ForegroundColor Green
