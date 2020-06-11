5a16314b96709d0b1a321919c4a1b564ae3d474d
Nick Wrobel <nickwrobel2@gmail.com>
2020-06-11 01:50:15 -0400

    updated audio spectrum image gen script to output the files into same directory as the audio file

--- Generate-AudioSpectrumImages.ps1
+++ Generate-AudioSpectrumImages.ps1
@@ -3,17 +3,19 @@ $ffmpegFilepath = Join-Path $ffmpegProgramDir -ChildPath 'ffmpeg.exe'
 
 # Must add \\?\ prefix to the directory path if any filepaths may be longer than 260 characters
 $audioFilesDir = '\\?\Z:\Music Library\!new-additions-wip-stage-temp'
-$imageOutputDir = 'D:\Temp\audio-spectrum-images-out'
 
-if (-not (Test-Path $imageOutputDir)) {
-    New-Item $imageOutputDir -ItemType Directory | Out-Null
-}
+# $imageOutputDir = 'D:\Temp\audio-spectrum-images-out'
+# if (-not (Test-Path $imageOutputDir)) {
+#     New-Item $imageOutputDir -ItemType Directory | Out-Null
+# }
 
 $audioFiles = Get-ChildItem -Path $audioFilesDir -Recurse -Include ('*.flac', '*.mp3', '*.m4a')
 
+# For each audio file, generate and output a spectrum image file into same directory that contains that audio file
 foreach ($audioFile in $audioFiles) {
-    $imageOutputFilename = "$($audioFile.Name)-SPEKIMG.jpg" 
-    $imageOutputFilepath = Join-Path $imageOutputDir -ChildPath $imageOutputFilename 
+    #$audioFileDir = (Get-Item $audioFile.PSParentPath).FullName
+    $imageOutputFilename = "$($audioFile.Name)-SPEKIMG.png" 
+    $imageOutputFilepath = "$($audioFile.DirectoryName)\$($imageOutputFilename)"
 
     Start-Process $ffmpegFilepath -ArgumentList ('-i', "`"$($audioFile.FullName)`"", '-lavfi', 'showspectrumpic=mode=combined:color=rainbow:s=1920x1024', "`"$($imageOutputFilepath)`"") -Wait -NoNewWindow
 }
