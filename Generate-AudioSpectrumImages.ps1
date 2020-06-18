6ad8bc0c286795d71c878985f180686ab9b34ddb
Nick Wrobel <nickwrobel2@gmail.com>
2020-06-17 21:14:49 -0400

    modified audio spectrum gen script to put all files into one folder

--- Generate-AudioSpectrumImages.ps1
+++ Generate-AudioSpectrumImages.ps1
@@ -2,14 +2,15 @@ $ffmpegProgramDir = 'C:\Program Files\ffmpeg-20200610-9dfb19b-win64-static\bin'
 $ffmpegFilepath = Join-Path $ffmpegProgramDir -ChildPath 'ffmpeg.exe'
 
 # Must add \\?\ prefix to the directory path if any filepaths may be longer than 260 characters
-$audioFilesDir = '\\?\Z:\Music Library\!new-additions-wip-stage-temp'
+$audioFilesDir = '\\?\Z:\Music Library\!new-additions-wip-stage-temp\!quality-fix-redownloads-wip-temp'
+$outputDir = 'D:\Temp\spectrums-quality-fix'
 
 $audioFiles = Get-ChildItem -Path $audioFilesDir -Recurse -Include ('*.flac')
 
 # For each audio file, generate and output a spectrum image file into same directory that contains that audio file
 foreach ($audioFile in $audioFiles) {
     $imageOutputFilename = "$($audioFile.Name)-SPEKIMG.png" 
-    $imageOutputFilepath = "$($audioFile.DirectoryName)\$($imageOutputFilename)"
+    $imageOutputFilepath = Join-Path $outputDir -ChildPath $imageOutputFilename
 
     if (Test-Path $imageOutputFilepath) {
         Write-Host "Audio spectrum image file already exists for this audio file: $($imageOutputFilename)"
@@ -18,3 +19,16 @@ foreach ($audioFile in $audioFiles) {
         Start-Process $ffmpegFilepath -ArgumentList ('-i', "`"$($audioFile.FullName)`"", '-lavfi', 'showspectrumpic=mode=combined:color=rainbow:s=1920x1024', "`"$($imageOutputFilepath)`"") -Wait -NoNewWindow
     }
 }
+
+
+# foreach ($audioFile in $audioFiles) {
+#     $imageOutputFilename = "$($audioFile.Name)-SPEKIMG.png" 
+#     $imageOutputFilepath = "$($audioFile.DirectoryName)\$($imageOutputFilename)"
+
+#     if (Test-Path $imageOutputFilepath) {
+#         Write-Host "Audio spectrum image file already exists for this audio file: $($imageOutputFilename)"
+    
+#     } else {
+#         Start-Process $ffmpegFilepath -ArgumentList ('-i', "`"$($audioFile.FullName)`"", '-lavfi', 'showspectrumpic=mode=combined:color=rainbow:s=1920x1024', "`"$($imageOutputFilepath)`"") -Wait -NoNewWindow
+#     }
+# }
